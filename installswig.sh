d4b5f537f3f1c43e62fa680337125f9577ccb5ab
Roman T <roooman30@gmail.com>
2024-02-23 16:21:21 +0100

    add installswig.sh

---
+++ installswig.sh
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2012-2024 Arne Schwabe
+# Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
+#
+
+apt-get install swig3.0
+ln -s /usr/bin/swig3.0 /usr/bin/swig
